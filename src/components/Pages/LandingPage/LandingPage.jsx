5bb7acadc2c720baf04fa8675464c65fa3f62973
Brock Benson <brockjbenson@outlook.com>
2023-05-01 16:45:12 -0500

    working on landing page

--- src/components/Pages/LandingPage/LandingPage.jsx
+++ src/components/Pages/LandingPage/LandingPage.jsx
@@ -10,6 +10,20 @@ function LandingPage() {
   return (
     <div className="bg-landingImg h-screen bg-no-repeat bg-auto bg-center">
       <Nav />
+      <div className="w-[100%]">
+        <div className="w-[450px] mx-auto flex-col justify-center items-center">
+          <h1 className="text-blue-950 font-extrabold text-5xl">
+            Welcome to the Northern Lights Poker Tour
+          </h1>
+          <p className="text-2xl pt-6">
+            Build your bankroll and your friendships with the NLPT. Click get
+            started to learn more.
+          </p>
+          <button className="bg-blue-950 text-white rounded-full  p-2 px-4">
+            Get Started
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
