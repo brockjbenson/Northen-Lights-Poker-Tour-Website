01e01ccba9925abe7b7527357f5c28538932658c
Brock Benson <brockjbenson@outlook.com>
2023-05-01 16:50:18 -0500

    working on landing message

--- src/components/Pages/LandingPage/LandingPage.jsx
+++ src/components/Pages/LandingPage/LandingPage.jsx
@@ -11,7 +11,7 @@ function LandingPage() {
     <div className="bg-landingImg h-screen bg-no-repeat bg-auto bg-center">
       <Nav />
       <div className="w-[100%]">
-        <div className="w-[450px] mx-auto flex-col justify-center items-center">
+        <div className="w-[450px] mx-auto flex-col">
           <h1 className="text-blue-950 font-extrabold text-5xl">
             Welcome to the Northern Lights Poker Tour
           </h1>
@@ -19,7 +19,7 @@ function LandingPage() {
             Build your bankroll and your friendships with the NLPT. Click get
             started to learn more.
           </p>
-          <button className="bg-blue-950 text-white rounded-full  p-2 px-4">
+          <button className="bg-blue-950 text-white rounded-full items-center p-2 px-4">
             Get Started
           </button>
         </div>
