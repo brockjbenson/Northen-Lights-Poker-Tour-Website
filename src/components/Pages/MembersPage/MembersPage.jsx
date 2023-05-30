c2e534d7fc0dc19885f80165b8d002584dac5c10
Brock Benson <brockjbenson@outlook.com>
2023-05-30 12:04:30 -0500

    working on member list layout

--- src/components/Pages/MembersPage/MembersPage.jsx
+++ src/components/Pages/MembersPage/MembersPage.jsx
@@ -1,5 +1,6 @@
 import { useSelector } from "react-redux";
 import Nav from "../../Shared/Nav/Nav";
+import "../MembersPage/MembersPage.css";
 
 export default function MembersPage() {
   const members = useSelector((store) => store.membersReducer.memberInfo);
@@ -10,15 +11,22 @@ export default function MembersPage() {
       <div className="member-page-container">
         <Nav />
         <div className="members-section">
-          <header>
+          <header className="members-header">
             <h1>Our Members</h1>
           </header>
           <div className="members-list">
             {members.map((member, id) => {
               return (
                 <ul key={id}>
-                  <li>{member.name}</li>
-                  <li>{member.image}</li>
+                  <header className="member-name">
+                    <li>{member.name}</li>
+                  </header>
+                  <div className="member-info">
+                    <li>
+                      {member.birthday} Age {member.age}
+                    </li>
+                    <li>Birthplace {member.birthplace}</li>
+                  </div>
                 </ul>
               );
             })}
